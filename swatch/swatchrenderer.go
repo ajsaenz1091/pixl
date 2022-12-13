ae361f36370056c4df393d6b7088ce4e02a68bcc
Alberto Gonzalez <ajsaenz1091@gmail.com>
2022-12-13 17:35:59 -0500

    implement swatchrenderer functions

---
+++ swatch/swatchrenderer.go
@@ -0,0 +1,44 @@
+package swatch
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"zerotomastery.io/pixl/apptype"
+
+	"image/color"
+)
+
+type SwatchRenderer struct {
+	square canvas.Rectangle
+	objects []fyne.CanvasObject
+	parent *Swatch
+}
+
+//Implementing Widget interface
+func (renderer *SwatchRenderer) MinSize() fyne.Size {
+	return renderer.square.MinSize()
+}
+
+func (renderer *SwatchRenderer) Layout(size fyne.Size) {
+	renderer.objects[0].Resize(size)
+}
+
+func (renderer *SwatchRenderer) Refresh() {
+	renderer.Layout(fyne.NewSize(20, 20))
+	renderer.square.FillColor = renderer.parent.Color
+	if renderer.parent.Selected {
+		renderer.square.StrokeWidth = 3
+		renderer.square.StrokeColor = color.NRGBA{255,255,255,255}
+		renderer.objects[0] = &renderer.square
+	} else {
+		renderer.square.StrokeWidth = 0
+		renderer.objects[0] = &renderer.square
+	}
+	canvas.Refresh(renderer.parent)
+}
+
+func (renderer *SwatchRenderer) Objects() []fyne.CanvasObject {
+	return renderer.objects[0]
+} 
+
+func (renderer *SwatchRenderer) Destroy() []fyne.CanvasObject {} 
