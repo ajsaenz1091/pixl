d1d0936a0616aaad6c5b288f098acea06a246e2a
Alberto Gonzalez <ajsaenz1091@gmail.com>
2022-12-13 14:52:43 -0500

    swatch initial setup

---
+++ swatch/swatch.go
@@ -0,0 +1,34 @@
+package swatch
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+	"zerotomastery.io/pixl/apptype"
+
+	"image/color"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected bool
+	Color color.Color 
+	SwatchIndex int
+	clickHandler func(s *Swatch)
+}
+
+func (s *Swatch) SetColor(c color.Color) {
+	s.Color = c
+	s.Refresh()
+}
+
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
+	swatch := &Swatch {
+		Selected: false,
+		Color: color,
+		ClickHandler: clickHandler,
+		SwatchIndex: swatchIndex,
+	}
+	swatch.ExtendBaseWidget(swatch)
+	return swatch
+}
