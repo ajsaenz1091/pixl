59ecb9f62593f909ba0e6b609792799154eb8195
Alberto Gonzalez <ajsaenz1091@gmail.com>
2022-12-14 21:29:22 -0500

    implement brush interface

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module zerotomastery.io/pixl
 go 1.19
 
 require (
-	fyne.io/fyne v1.4.3
 	fyne.io/fyne/v2 v2.2.4
 	github.com/lusingander/colorpicker v0.7.1
 )
--- pxcanvas/mouse.go
+++ pxcanvas/mouse.go
@@ -3,7 +3,7 @@ package pxcanvas
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/driver/desktop"
-	// "zerotomastery.io/pixl/pxcanvas/brush"
+	"zerotomastery.io/pixl/pxcanvas/brush"
 )
 
 func (pxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
@@ -12,15 +12,15 @@ func (pxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
 }
 
 func (pxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
-	// if x, y := pxCanvas.MouseToCanvasXY(ev); x != nil && y != nil {
-	// 	brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
-	// 	cursor := brush.Cursor(pxCanvas.PxCanvasConfig, pxCanvas.appState.BrushType, ev, *x, *y)
-	// 	pxCanvas.renderer.SetCursor(cursor)
-	// 	pxCanvas.showMouse = false
-	// } else {
-	// 	pxCanvas.showMouse = true
-	// 	pxCanvas.renderer.SetCursor(make([]fyne.CanvasObject, 0))
-	// }
+	if x, y := pxCanvas.MouseToCanvasXY(ev); x != nil && y != nil {
+		brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+		cursor := brush.Cursor(pxCanvas.PxCanvasConfig, pxCanvas.appState.BrushType, ev, *x, *y)
+		pxCanvas.renderer.SetCursor(cursor)
+		pxCanvas.showMouse = false
+	} else {
+		pxCanvas.showMouse = true
+		pxCanvas.renderer.SetCursor(make([]fyne.CanvasObject, 0))
+	}
 	pxCanvas.TryPan(pxCanvas.mouseState.previousCoord, ev)
 	pxCanvas.Refresh()
 	pxCanvas.mouseState.previousCoord = &ev.PointEvent
@@ -29,7 +29,8 @@ func (pxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
 func (pxCanvas *PxCanvas) MouseIn(ev *desktop.MouseEvent) {}
 func (pxCanvas *PxCanvas) MouseOut()                      {}
 
-//	func (pxCanvas *PxCanvas) MouseDown(ev *desktop.MouseEvent) {
-//		brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
-//	}
+func (pxCanvas *PxCanvas) MouseDown(ev *desktop.MouseEvent) {
+	brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+}
+
 func (pxCanvas *PxCanvas) MouseUp(ev *desktop.MouseEvent) {}
